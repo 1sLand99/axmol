8261cca14ddb52a78835eb6e04e40616cfcb3958
halx99 <halx99@live.com>
2026-04-10 22:45:43 +0800

    Improve winrt code style

--- axmol/rhi/d3d12/RenderContext12.cpp
+++ axmol/rhi/d3d12/RenderContext12.cpp
@@ -33,12 +33,7 @@
 #include "axmol/base/Logging.h"
 
 #if AX_TARGET_PLATFORM == AX_PLATFORM_WINRT
-#    include <windows.ui.xaml.media.dxinterop.h>
-#    include <windows.ui.xaml.controls.h>
-#    include <windows.ui.core.h>
-#    include <windows.foundation.h>
-#    include <wrl/event.h>
-#    include <wrl/implements.h>
+#    include "axmol/platform/winrt/ThreadUtils.h"
 #endif
 
 namespace ax::rhi::d3d12
@@ -87,98 +82,6 @@ static DXGI_FORMAT toDxgiIndexFormat(IndexFormat fmt)
         return DXGI_FORMAT_R32_UINT;
     }
 }
-
-#if AX_TARGET_PLATFORM == AX_PLATFORM_WINRT
-
-using ICoreDispatcher    = ABI::Windows::UI::Core::ICoreDispatcher;
-using IDispatchedHandler = ABI::Windows::UI::Core::IDispatchedHandler;
-using IAsyncAction       = ABI::Windows::Foundation::IAsyncAction;
-using ISwapChainPanel    = ABI::Windows::UI::Xaml::Controls::ISwapChainPanel;
-using IDependencyObject  = ABI::Windows::UI::Xaml::IDependencyObject;
-using IUIElement         = ABI::Windows::UI::Xaml::IUIElement;
-
-// Creates a COM/WinRT callback object for the specified interface type (_Ty)
-// that is implemented with Free‑Threaded Marshaler (FtmBase) support.
-//
-// This helper wraps Microsoft::WRL::Callback with an Implements<> type that
-// includes FtmBase, making the resulting object agile across threads.
-// This is especially useful when passing the handler to APIs like
-// ICoreDispatcher::RunAsync, which may invoke the callback on a different thread.
-//
-// Template parameters:
-//   _Ty  - The COM/WinRT interface type to implement (e.g. ABI::Windows::UI::Core::IDispatchedHandler)
-//   _Fty - The callable type (lambda, functor, etc.) providing the implementation
-//
-// Parameters:
-//   func - A callable object implementing the interface's Invoke method
-//
-// Returns:
-//   A Microsoft::WRL::ComPtr-compatible callback object implementing _Ty with FTM support.
-template <typename _Ty, typename _Fty>
-static auto makeFtmHandler(_Fty&& func)
-{
-    using Impl = Microsoft::WRL::Implements<Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>, _Ty,
-                                            Microsoft::WRL::FtmBase>;
-    return Microsoft::WRL::Callback<Impl>(std::forward<_Fty>(func));
-}
-
-template <typename _Fty>
-static HRESULT runOnUIThread(const ComPtr<ICoreDispatcher>& dispatcher, _Fty&& func)
-{
-    using namespace ABI::Windows::UI::Core;
-
-    boolean hasThreadAccess = FALSE;
-    HRESULT hr              = dispatcher->get_HasThreadAccess(&hasThreadAccess);
-    if (FAILED(hr))
-        return hr;
-
-    if (hasThreadAccess)
-    {
-        return func();
-    }
-
-    struct AutoHandle
-    {
-        explicit AutoHandle(HANDLE h) : _h(h) {}
-        ~AutoHandle()
-        {
-            if (_h)
-                ::CloseHandle(_h);
-        }
-        HANDLE get() const { return _h; }
-        explicit operator bool() const { return _h != nullptr; }
-
-    private:
-        HANDLE _h;
-    };
-
-    AutoHandle waitEvent{::CreateEventExW(nullptr, nullptr, CREATE_EVENT_MANUAL_RESET, EVENT_ALL_ACCESS)};
-    if (!waitEvent)
-        return E_FAIL;
-
-    HRESULT hr2 = E_FAIL;
-
-    auto handler = makeFtmHandler<IDispatchedHandler>([&]() -> HRESULT {
-        hr2 = func();
-        ::SetEvent(waitEvent.get());
-        return S_OK;
-    });
-
-    ComPtr<IAsyncAction> asyncAction;
-    hr = dispatcher->RunAsync(CoreDispatcherPriority_Normal, handler.Get(), &asyncAction);
-    if (FAILED(hr))
-        return hr;
-
-    auto waitResult = ::WaitForSingleObjectEx(waitEvent.get(), 10 * 1000, TRUE);
-    if (waitResult != WAIT_OBJECT_0)
-    {
-        std::terminate();
-        return E_FAIL;
-    }
-
-    return hr2;
-}
-#endif
 
 uint64_t GPUFence::wait() const
 {
@@ -261,15 +164,15 @@ RenderContextImpl::RenderContextImpl(DriverImpl* driver, SurfaceHandle surface) 
     do
     {
         ComPtr<IUnknown> surfaceHold(static_cast<IUnknown*>(surface));
-        ComPtr<ISwapChainPanel> swapChainPanel;
+        ComPtr<winrt::ISwapChainPanel> swapChainPanel;
         hr = surfaceHold.As(&swapChainPanel);
         AX_BREAK_IF(FAILED(hr));
 
-        ComPtr<IDependencyObject> swapChainPanelDependencyObject;
+        ComPtr<winrt::IDependencyObject> swapChainPanelDependencyObject;
         hr = swapChainPanel.As(&swapChainPanelDependencyObject);
         AX_BREAK_IF(FAILED(hr));
 
-        ComPtr<ICoreDispatcher> dispatcher;
+        ComPtr<winrt::ICoreDispatcher> dispatcher;
         hr = swapChainPanelDependencyObject->get_Dispatcher(dispatcher.GetAddressOf());
         AX_BREAK_IF(FAILED(hr));
 
@@ -278,12 +181,12 @@ RenderContextImpl::RenderContextImpl(DriverImpl* driver, SurfaceHandle surface) 
         AX_BREAK_IF(FAILED(hr));
 
         ABI::Windows::Foundation::Size panelSize;
-        ComPtr<IUIElement> uiElement;
+        ComPtr<winrt::IUIElement> uiElement;
         hr = swapChainPanel.As(&uiElement);
         AX_BREAK_IF(FAILED(hr));
 
         Vec2 renderScale;
-        hr = runOnUIThread(dispatcher, [&panelSize, &renderScale, uiElement, swapChainPanel] {
+        hr = winrt::runOnUIThread(dispatcher, [&panelSize, &renderScale, uiElement, swapChainPanel] {
             HRESULT hr1 = uiElement->get_RenderSize(&panelSize);
             if (FAILED(hr1))
                 return hr1;
@@ -320,7 +223,7 @@ RenderContextImpl::RenderContextImpl(DriverImpl* driver, SurfaceHandle surface) 
         AX_BREAK_IF(FAILED(hr));
         swapchain1.As(&swapchain);
 
-        hr = runOnUIThread(dispatcher, [swapChainPanelNative, swapchain1] {
+        hr = winrt::runOnUIThread(dispatcher, [swapChainPanelNative, swapchain1] {
             return swapChainPanelNative->SetSwapChain(swapchain1.Get());
         });
         AX_BREAK_IF(FAILED(hr));
@@ -904,8 +807,8 @@ void RenderContextImpl::prepareDrawing(ID3D12GraphicsCommandList* cmd)
         int maxSlot = -1;
         for (auto& [bindingIndex, bindingSet] : textureBindingSets)
         {
-            const auto count = bindingSet.texs.size();
-            for (size_t i = 0; i < count; ++i)
+            const auto count = static_cast<int>(bindingSet.texs.size());
+            for (int i = 0; i < count; ++i)
             {
                 const int slot = bindingIndex + i;
                 if (maxSlot < slot)
